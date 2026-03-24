0330cf0dad56d264938592f400712c1f0fb27e64
Leonardo Silva <leonardosk1357@gmail.com>
2026-03-24 10:52:57 -0300

    Estrutura Inicial

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Cadastro e Gerenciamento de Ocorrências Estudantis</title>
+    <link rel="stylesheet" href="src/style/main.css">
+</head>
+<body>
+    <h1>Sistema de Cadastro e Gerenciamento de Ocorrências Estudantis</h1>
+
+    <script src="src/script/script.js"></script>
+</body>
+</html>
